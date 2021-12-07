33c519e80a67cc9b2f474cefdc8ffd433a33f3e7
wudueuk <wudueuk@gmail.com>
2021-12-07 20:37:43 +0200

    add index.js, main.js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import slideMenu from './main.js'
+
+slideMenu({
+	openBtn: '.header__burger-btn',
+	menu: '.navigation',
+	classActiveMenu: 'navigation_active',
+	closeTrigger: '.navigation__link, .navigation__close',
+});
